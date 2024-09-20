4e9d29b23984618aa832ca05f225767362fe3391
Adam-Elmi <Adamcade123@gmail.com>
2024-09-20 20:10:08 +0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: ["./*.{html,js,css}"],
   theme: {
     extend: {
       colors: {
